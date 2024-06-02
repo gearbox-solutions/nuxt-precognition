4b24ea67417eaa67a42a6d1797e77881b1b1315e
David Nahodyl <david@gearboxgo.com>
2024-06-02 15:07:07 -0400

    cleared homepage

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -1,41 +1,8 @@
 <script setup lang="ts">
-// const form = useForm({
-//   description: '',
-// })
-
-const form = usePrecognitiveForm('post', '/api/todo', {
-  description: '',
-})
-
-const submitForm = async () => {
-  form.post('/api/todo')
-}
 </script>
 
 <template>
   <div>
-    <pre>{{ form }}</pre>
-    <form
-      class="space-y-4"
-      @submit.prevent="submitForm"
-    >
-      <div class="space-x-4">
-        <LabeledInput
-          id="description"
-          v-model="form.description"
-          label="Description"
-          type="text"
-          name="description"
-          :errors="form.errors.description"
-          @change="form.validate('description')"
-        />
-      </div>
-
-      <div>
-        <ButtonPrimary :disabled="form.processing">
-          Submit
-        </buttonprimary>
-      </div>
-    </form>
+    Home
   </div>
 </template>
