0b59464e5905ddc835196e4fef377336d65d4129
David Nahodyl <david@gearboxgo.com>
2024-06-03 15:57:35 -0400

    simplified

--- playground/server/api/todo-precog/index.post.ts
+++ playground/server/api/todo-precog/index.post.ts
@@ -5,26 +5,23 @@ const todoRequestSchema = z.object({
   age: z.number().min(18, 'Must be at least 18 years old'),
 })
 
-async function handler(event) {
-  const validated = await getValidatedInput(event, todoRequestSchema)
+export default definePrecognitionEventHandler(
+  todoRequestSchema,
+  async (event) => {
+    const validated = await getValidatedInput(event, todoRequestSchema)
 
-  // do something with the body
-  const newTodo = {
-    id: 1,
-    description: validated.description,
-  }
+    // do something with the body
+    const newTodo = {
+      id: 1,
+      description: validated.description,
+    }
 
-  // simulate a slow response to show the loading state o the front-end
-  await sleep(1000)
+    // simulate a slow response to show the loading state o the front-end
+    await sleep(1000)
 
-  return newTodo
-}
+    return newTodo
+  })
 
 function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
-
-export default handlePrecognitionRequest(
-  handler,
-  todoRequestSchema,
-)
