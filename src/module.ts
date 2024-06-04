bf47b6e56db70c090bdf5d16e44c69c0ee56511b
David Nahodyl <david@gearboxgo.com>
2024-06-04 10:47:48 -0400

    lodash vite config

--- src/module.ts
+++ src/module.ts
@@ -15,5 +15,21 @@ export default defineNuxtModule<ModuleOptions>({
 
     addImportsDir(resolver.resolve('./runtime/composables'))
     addServerImportsDir(resolver.resolve('./runtime/server/utils'))
+
+    _nuxt.hook('vite:extendConfig', (config) => {
+      config.optimizeDeps = config.optimizeDeps || {}
+      config.optimizeDeps.include = config.optimizeDeps.include || []
+      config.optimizeDeps.exclude = config.optimizeDeps.exclude || []
+
+      const lodashIndex
+        = config.optimizeDeps.exclude.indexOf('lodash')
+      if (lodashIndex > -1) {
+        config.optimizeDeps.exclude.splice(lodashIndex, 1)
+      }
+
+      if (!config.optimizeDeps.include.includes('lodash')) {
+        config.optimizeDeps.include.push('lodash')
+      }
+    })
   },
 })
