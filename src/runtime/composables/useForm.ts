6f13d29886a81ed6891b8c68a367825438e4027b
David Nahodyl <david@gearboxgo.com>
2024-06-04 10:22:45 -0400

    import from js

--- src/runtime/composables/useForm.ts
+++ src/runtime/composables/useForm.ts
@@ -1,4 +1,4 @@
-import isEqual from 'lodash.isequal/index.js'
+import { isEqual } from 'lodash.isequal/index.js'
 import { reactive, watch } from 'vue'
 import type { NitroFetchRequest } from 'nitropack'
 import type FormDataConvertible from '~/types/FormDataconvertible'
